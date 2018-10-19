8a9aaf63a00228228300ef29487f0a81d7bc9176
Sean Glover <sglover@ybp.com>
2018-10-19 17:54:39 -0400

    Just setting up page and style template and route tests

--- source/js/app.js
+++ source/js/app.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import combinedReducer from './reducers/combinedReducer'
 import TestContainer from './containers/testContainer';
+import TestRoutable from './components/testRoutable';
 
 let initialState = {}
 let store = createStore(combinedReducer, initialState);
@@ -11,7 +13,12 @@ let store = createStore(combinedReducer, initialState);
 ReactDOM.render(
     <Provider store={store}>
         {/* Insert container component here */}
-        <TestContainer />
+        <Router>
+            <Switch>
+                <Route exact path="/" component={TestContainer} />
+                <Route exact path="/Routable" component={TestRoutable} />
+            </Switch>
+        </Router>
     </Provider>,
     document.getElementById('app')
 );
---
+++ source/js/components/testRoutable.js
@@ -0,0 +1,7 @@
+﻿import React, { Component } from 'react';
+
+let TestRoutable = () => (
+        <h1>Hello Route!</h1>
+)
+
+export default TestRoutable
